b529e2b368232c651b4f03517e73d94138c8635e
Gun Gun Priatna <gungunpriatna02@gmail.com>
2018-11-20 11:25:45 +0700

    update App.vue and store.js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Search />
+    <User />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Search from './components/Search';
+import User from './components/User';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
-}
+    User,
+    Search
+  }  
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+#app
+  font-family 'Avenir', Arial, Helvetica, sans-serif
+  font-smoothing antialiased
+  margin 10px
 </style>
